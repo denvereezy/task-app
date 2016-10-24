65807d883b6553e43f5c14d0352031c06e1fe46d
denver <denver@projectcodex.co>
2016-10-24 10:15:00 +0200

    updated

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,24 +1,26 @@
-@extends('layouts.app')
-@section('content')
+@extends('layouts.app') @section('content')
 
 <div class="container-fluid">
+    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
 
-    <form action="/email/send/{{$task->id}}" method="POST">
-        <div class="form-group">
-            {{ method_field('PATCH') }}
-             {!! csrf_field() !!}
-            <label>Task</label>
-            <textarea name="name" rows="8" cols="40" disabled>{{$task->name}}"</textarea>
-        </div>
-        <div class="form-group">
-            <label>To:</label> {{ csrf_field() }}
-            <input type="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label>Subject:</label> {{ csrf_field() }}
-            <input type="text" name="subject">
-        </div>
-        <input class="btn btn-primary" type="submit" value="Send">
-    </form>
+        <form class="col-md-8" action="/email/send/{{$task->id}}" method="POST">
+            <div class="form-group">
+                {{ method_field('PATCH') }} {!! csrf_field() !!}
+                <label>Task</label>
+                <p>{{$task->name}}</p>
+            </div>
+            <div class="form-group">
+                <label>To:</label> {{ csrf_field() }}
+                <input class="form-control" type="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label>Subject:</label> {{ csrf_field() }}
+                <input class="form-control" type="text" name="subject">
+            </div>
+            <input class="btn btn-success" type="submit" value="Send">
+            <a class="btn btn-primary pull-right" href="/tasks">Back To Tasks</a>
+
+        </form>
+    </div>
 </div>
 @endsection
