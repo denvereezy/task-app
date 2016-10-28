20e4d2fad6583b0479cdbd3917332f207b0566c9
denver <denver@projectcodex.co>
2016-10-28 14:14:16 +0200

    user policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     */
+    public function destroy(User $user)
+    {
+        return $this->user = $user;
+    }
+}
